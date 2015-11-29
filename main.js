1cabe727778bc7672b94825cd93d5c45ce0f26a8
Amal Nanavati <arnanava@andrew.cmu.edu>
2015-11-29 02:19:12 -0500

    added more the dropdown question, also added some game mechanics such as how to move to next exercise

--- main.js
+++ main.js
@@ -12,12 +12,30 @@ var main = function (ex) {
      */
     var questionType = 0;
 
+    /* Which question number we are on (useful if we only want 10 questions) */
+    var questionNum = 1;
+
+    /* Used in question type 0, this is a list of dropdowns and the correct 
+     * answer for each dropdown 
+     */
+    var dropdownList = undefined;
+
+    /* Keeps track of whether the correct answer is being displayed or not
+     */
+    var isCorrectAnswerBeingDisplayed = false;
+    var oldAnswers = []; // Used in Q type 0 to restore old answers after unshowing CA
+
+    /* Shows the appropriate question type
+     */
     var showQuestion = function () {
         switch (questionType) {
             case 0:
                 /* Create the code well */
                 var x = 50;
-                var y = 50;
+                var y = 100;
+                var margin = 50;
+                var width = ex.width()-x-margin;
+                var height = ex.height()-y-margin;
                 var code = "def f(x<span>'_1'</span>\n    return <span>'_2'</span>";
                 var dropdownInfo = {"'_1'" : {":)" : {"feedback" : "Should the colon be inside or outside the parenthesis?",
                                                     "correct" : false,
@@ -28,7 +46,7 @@ var main = function (ex) {
                                             ");" : {"feedback" : "Check your colon.",
                                                     "correct" : false,
                                                     "default" : true},
-                                            ")" : {"feedback" : "What goes att he end of every function definition?",
+                                            ")" : {"feedback" : "What goes at the end of every function definition?",
                                                     "correct" : false,
                                                     "default" : false}},
                                     "'_2'" : {"5*x;" : {"feedback" : "What, if anything, goes at the end of every line?",
@@ -37,23 +55,142 @@ var main = function (ex) {
                                             "5x" : {"feedback" : "You need an operator between the 5 and the x.",
                                                     "correct" : false,
                                                     "default" : false},
-                                            "x+x+x+x+x" : {"feedback" : "",
+                                            "x+x+x+x+x" : {"feedback" : "Python does not use any symbol to indicate the end of a linr.",
                                                     "correct" : true,
                                                     "default" : false},
                                             "5(x)" : {"feedback" : "You need an operator between the 5 and the x.",
                                                     "correct" : false,
                                                     "default" : false}}};
-                var codeWell = createCodeWellWithOptions(ex, x, y, code, dropdownInfo, true, true);
+                dropdownList = createCodeWellWithOptions(ex, x, y, width, height, code, dropdownInfo, true, true);
 
                 /* Create the instructions/description of code */
-                
+                var text = "Write a function that takes in an integer and returns that integer multiplied by 5"
+                var header = ex.createParagraph(0,0,text,{size: "xlarge", width:ex.width(), textAlign:"center"});
+
                 break;
             case 1:
                 break;
             case 2:
                 break;
-        }
+        };
     };
+
+    /* Handles clicks to the submit button
+     */
+    var submit = function () {
+        switch (questionType) {
+            case 0:
+                /* Generate Feedback */
+                var numOfCorrectDropdowns = 0;
+                if (dropdownList === undefined) {
+                    throw "AMAL-DEFINED EXCEPTION: dropdownList is undefined";
+                };
+                for (var i = 0; i < dropdownList.length; i++) {
+                    var currText = dropdownList[i].dropdown.text();
+                    currText = currText.slice(0, currText.indexOf('<span class="caret">'));
+                    console.log(currText);
+                    if (currText.indexOf(dropdownList[i].correct) > -1) {
+                        numOfCorrectDropdowns = numOfCorrectDropdowns + 1;
+                    };
+                    /* Disable dropdown */
+                    dropdownList[i].dropdown.disable();
+                };
+                var beginning = "";
+                if (numOfCorrectDropdowns == dropdownList.length) beginning = "Congratulations!  "
+                if (numOfCorrectDropdowns >= dropdownList.length/2) beginning = "Good job!  "
+                var feedback = beginning.concat("You got ").concat(String(numOfCorrectDropdowns)).concat(" dropdowns correct out of ").concat(String(dropdownList.length)).concat(".  Press 'Next' to move on.");
+                ex.showFeedback(feedback);
+                /* Enable Display CA Button */
+                ex.chromeElements.displayCAButton.enable();
+                break;
+            case 1:
+                break;
+            case 2:
+                break;
+        };
+        /* Create the next button */
+        var x = ex.width()-100;
+        var y = ex.height()-50;
+        var nextButton = ex.createButton(x, y,"Next", {
+                                                        color: "white",
+                                                        size: "medium"
+                                                      });
+        nextButton.on("click", function () {
+            isCorrectAnswerBeingDisplayed = false;
+            questionNum++;
+            /* USE THIS SPACE TO CHANGE QUESTION TYPE OR TO CHECK IF YOU HAVE 
+             * REACHED THE MAX NUMBER OF QUESTIONS! 
+             */
+            showQuestion();
+            nextButton.remove();
+            /* Disable Display CA Button */
+            ex.chromeElements.displayCAButton.disable();
+        });
+    };
+
+    /* Handles clicks to the submit button
+     */
+    var displayCA = function () {
+        switch (questionType) {
+            case 0:
+                console.log(isCorrectAnswerBeingDisplayed);
+                if (isCorrectAnswerBeingDisplayed) {
+                    isCorrectAnswerBeingDisplayed = false;
+                    for (var i = 0; i < dropdownList.length; i++) {
+                        dropdownList[i].dropdown.text(oldAnswers[i]);
+                        console.log(dropdownList[i].dropdown.text());
+                    };
+                } else {
+                    isCorrectAnswerBeingDisplayed = true;
+                    for (var i = 0; i < dropdownList.length; i++) {
+                        oldAnswers[i] = dropdownList[i].dropdown.text();
+                        dropdownList[i].dropdown.text(dropdownList[i].correct);
+                        console.log(dropdownList[i].dropdown.text());
+                    };
+                };
+                break;
+            case 1:
+                break;
+            case 2:
+                break;
+        };
+    };
+
+    /* Turns on/off buttons and handlers
+     */
+    var setUp = function () {
+        switch (questionType) {
+            case 0:
+                ex.chromeElements.submitButton.enable();
+                ex.chromeElements.submitButton.off("click");
+                ex.chromeElements.submitButton.on("click", submit);
+                ex.chromeElements.displayCAButton.disable();
+                ex.chromeElements.displayCAButton.off("click");
+                ex.chromeElements.displayCAButton.on("click", displayCA);
+                ex.chromeElements.undoButton.disable();
+                ex.chromeElements.undoButton.off("click");
+                ex.chromeElements.undoButton.on("click", function () {});
+                ex.chromeElements.redoButton.disable();
+                ex.chromeElements.redoButton.off("click");
+                ex.chromeElements.redoButton.on("click", function () {});
+                ex.chromeElements.resetButton.enable();
+                ex.chromeElements.resetButton.off("click");
+                ex.chromeElements.resetButton.on("click", function () {});
+                ex.chromeElements.newButton.enable();
+                ex.chromeElements.newButton.off("click");
+                ex.chromeElements.newButton.on("click", function () {});
+                break;
+            case 1:
+                break;
+            case 2:
+                break;
+        };
+    };
+
+    var run = function () {
+        setUp();
+        showQuestion();
+    }
 
     function CodeCard(left,content){
         var code = {};
@@ -72,24 +209,24 @@ var main = function (ex) {
             code.well = ex.createCode(x,y,code.content,
                 {language:"python",
                  selectable:true,
-                  width:code.width
+                  width:code.width,
                   height:code.height}).on("click",function(){
-                  	code.clicked = true;
-                  	code.highlight();
-                  	if (code.left) {
-                  		currentIndentPrac.clicked = 1;
-                  		currentIndentPrac.rightCard.clicked = false;
-                  	}
-                  	else {
-                  		currentIndentPrac.clicked = -1;
-                  		currentIndentPrac.leftCard.clicked = false;
-                  	}
-                  	currentIndentPrac.redrawCard();}
-                  	);
+                    code.clicked = true;
+                    code.highlight();
+                    if (code.left) {
+                        currentIndentPrac.clicked = 1;
+                        currentIndentPrac.rightCard.clicked = false;
+                    }
+                    else {
+                        currentIndentPrac.clicked = -1;
+                        currentIndentPrac.leftCard.clicked = false;
+                    }
+                    currentIndentPrac.redrawCard();}
+                    );
             if (code.clicked) code.highlight();
         }
         code.clear = function(){
-        	ex.graphics.ctx.clearRect(code.x,code.y,code.width,code.height);
+            ex.graphics.ctx.clearRect(code.x,code.y,code.width,code.height);
             if (code.well != undefined){
                 code.well.remove();
             }
@@ -110,28 +247,28 @@ var main = function (ex) {
         q.clicked = 0;
         q.x = 
         q.drawQuestion = function(){
-        	ex.createParagraph(q.x,q.y,
-        		"Click on the code that correctly"+q.question);
+            ex.createParagraph(q.x,q.y,
+                "Click on the code that correctly"+q.question);
         }
         q.draw = function(){
-        	q.leftCard.draw();
-        	q.rightCard.draw();
-        	q.drawQuestion();
+            q.leftCard.draw();
+            q.rightCard.draw();
+            q.drawQuestion();
         }
         q.redrawCard = function(){
-        	if (q.clicked == 1){
-        		q.rightCard.clear();
-        		q.rightCard.draw();
-        	}
-        	else if (q.clicked == -1){
-        		q.leftCard.clear();
-        		q.leftCard.draw();
-        	}
+            if (q.clicked == 1){
+                q.rightCard.clear();
+                q.rightCard.draw();
+            }
+            else if (q.clicked == -1){
+                q.leftCard.clear();
+                q.leftCard.draw();
+            }
         }
         return q;
     }
-    
-    showQuestion();
+
+    run();
     
 }
 
@@ -142,9 +279,11 @@ var main = function (ex) {
  * of feedback for when the user chooses that option, the "correct" key
  * mapping to a bool representing whether that option is correct or not, and 
  * the "default" key mapping to a bool indicating whether that element is the
- * default or not.
+ * default or not.  Returns a list of dictionaries, where the key "dropdown" 
+ * maps to the dropdown, and the key "correct" maps to the correct answer for
+ * that dropdown.
  */
-var createCodeWellWithOptions = function (ex, x, y, code, dropdownInfo, showFeedbackIfCorrect, showFeedbackIfWrong) {
+var createCodeWellWithOptions = function (ex, x, y, width, height, code, dropdownInfo, showFeedbackIfCorrect, showFeedbackIfWrong) {
 
     /* Success and failure functions, which will be called when the user selects
      * an option on the dropdown
@@ -153,9 +292,9 @@ var createCodeWellWithOptions = function (ex, x, y, code, dropdownInfo, showFeed
         return function () {
             feedback = "Correct!  ".concat(feedback);
             var message = ex.alert(feedback, {
-                    fontSize: 18,
+                    fontSize: 20,
                     stay: true,
-                    opacity: 0.8
+                    color:"green"
             });
         };
     };
@@ -163,49 +302,55 @@ var createCodeWellWithOptions = function (ex, x, y, code, dropdownInfo, showFeed
         return function () {
             feedback = "Incorrect!  ".concat(feedback);
             var message = ex.alert(feedback, {
-                    fontSize: 18,
+                    fontSize: 20,
                     stay: true,
-                    opacity: 0.8
+                    color:"red"
             });
         };
     };
 
     /* Create the code well */
-    var codeWell = ex.createCode(x, y, code, {size:"large"});
+    var codeWell = ex.createCode(x, y, code, {size:"large", width:width, height:height});
 
     /* Create the dropdown */
+    var dropdownList = [];
     for (var substring in dropdownInfo) {
         if (dropdownInfo.hasOwnProperty(substring)) {
             /* Create the dropdown options object */
             var elements = {};
             var defaultStr = "";
+            var correctAnswer = undefined;
             for (var option in dropdownInfo[substring]) {
                 if (dropdownInfo[substring].hasOwnProperty(option)) {
                     var correct = dropdownInfo[substring][option]["correct"];
                     if (dropdownInfo[substring][option]["default"]) {
                         defaultStr = option;
-                    }
+                    };
                     var feedback = undefined;
                     if (correct) {
+                        correctAnswer = option;
                         if (showFeedbackIfCorrect) {
                             feedback = dropdownInfo[substring][option]["feedback"];   
-                        } 
+                        } ;
                         elements[option] = getSuccessFn(dropdownInfo[substring][option]["feedback"]);
                     } else {
                         if (showFeedbackIfWrong) {
                             feedback = dropdownInfo[substring][option]["feedback"];   
-                        } 
+                        } ;
                         elements[option] = getFailureFn(dropdownInfo[substring][option]["feedback"]);
-                    }
-                }
-            }
+                    };
+                };
+            };
 
             /* Create the dropdown */
             var codeDrop = ex.createDropdown(0,0,defaultStr,{elements: elements});
 
             /* Insert the dropdown into the codeWell */
             ex.insertDropdown(codeWell, substring, codeDrop);
-        }
-    }
-    return codeWell;
+
+            /* Add the dropdown and correct answer to the list */
+            dropdownList.push({"dropdown":codeDrop, "correct":correctAnswer});
+        };
+    };
+    return dropdownList;
 };
